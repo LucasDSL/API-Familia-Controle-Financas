5e7f3b2236776ad3074a617a6042231ebaa533f3
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-17 16:31:13 -0300

    Rota e método para ver detalhes de uma receita

--- src/routes/receitas.routes.js
+++ src/routes/receitas.routes.js
@@ -4,4 +4,5 @@ const { ReceitasController } = require("../controllers")
 const router = Router()
 router.post("/receitas", ReceitasController.cadastrarReceita)
 router.get("/receitas", ReceitasController.listarReceitas)
+router.get("/receitas/:id", ReceitasController.pegarReceita)
 module.exports = router
