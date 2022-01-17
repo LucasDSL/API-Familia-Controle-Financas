8a12c1adf4cb9e6cf4ff779ff5b000b5da77dae7
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-17 14:56:27 -0300

    Melhora na mensagem de erro ao não preencher campo requerido no body

--- src/errors/campoNaoEncontrado.js
+++ src/errors/campoNaoEncontrado.js
@@ -1,7 +1,6 @@
 class CampoNaoEncontrado extends Error {
   constructor(campo) {
-    const mensagem = `É necessário preencher os campos ["descricao", "valor", "data"]\n
-    O campo ${campo} não foi preenchido!`
+    const mensagem = `É necessário preencher os campos 'descricao', 'valor' e 'data'. O campo ${campo} não foi preenchido!`
     super(mensagem)
     this.name = "CampoNaoEncontrado"
     this.id = 1
