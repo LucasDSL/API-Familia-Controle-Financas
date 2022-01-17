b62c6f8a29d5fa3c69247483ebec5f4353ffc085
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-17 15:04:24 -0300

    Atualização de status code para campoNaoEncontrado

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,10 +2,11 @@ const Errors = require("../errors")
 module.exports = (erro, req, res, next) => {
   let status = 0
   if (erro instanceof Errors.CampoNaoEncontrado) {
-    status = 404
+    status = 400
   }
 
   res.status(status).send({
+    nomeErro: erro.name,
     mensagem: erro.message,
     id: erro.id,
   })
