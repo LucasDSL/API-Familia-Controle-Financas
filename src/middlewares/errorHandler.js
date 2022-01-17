7c2e78bc3fc53be9eef7717803414228984b5d2d
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-17 16:55:13 -0300

    Status padrão pra erro no servidor, rota e método para deleção de receita

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const Errors = require("../errors")
 module.exports = (erro, req, res, next) => {
-  let status
+  let status = 500
   if (erro instanceof Errors.CampoNaoEncontrado) {
     status = 400
   }
