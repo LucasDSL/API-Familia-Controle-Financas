8c964e7ee82af25a719dbb2d7a4b3480d94d8b5f
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-17 15:11:50 -0300

    Controller para adição de uma nova receita

--- src/controllers/receitas.controller.js
+++ src/controllers/receitas.controller.js
@@ -1,6 +1,20 @@
+const CampoNaoEncontrado = require("../errors/campoNaoEncontrado")
+const ModelReceitas = require("../models").Receitas
 class ReceitasController {
-  static async cadastrarReceita(req, res) {
-    
+  static async cadastrarReceita(req, res, next) {
+    const novaReceita = req.body
+    try {
+      const camposObrigatorios = ["descricao", "valor", "data"]
+      camposObrigatorios.forEach((campo) => {
+        if (!novaReceita[campo]) {
+          throw new CampoNaoEncontrado(campo)
+        }
+      })
+      const receitaAdicionada = await ModelReceitas.create(novaReceita)
+      res.status(201).send(novaPessoa)
+    } catch (error) {
+      return next(error)
+    }
   }
 }
 
